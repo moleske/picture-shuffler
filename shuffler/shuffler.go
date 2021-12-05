b7da9e232f1b7447898c5178b1315f6162308f2e
M. Oleske <michael@oleske.engineer>
2021-12-05 14:58:07 -0800

    run infinetly

--- shuffler/shuffler.go
+++ shuffler/shuffler.go
@@ -26,37 +26,41 @@ func Shuffle(cmd *cobra.Command, args []string) {
 }
 
 func changeContent(fyneWindow fyne.Window, path string) {
-	err := filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
+	var err error
+	for _ = 1; true; {
+		err = filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
+			if err != nil {
+				return err
+			}
 
-		if d.IsDir() {
+			if d.IsDir() {
+				return nil
+			}
+
+			fmt.Println(path)
+			openFile, err := os.Open(path)
+			if err != nil {
+				fmt.Println("some error on os.open***" + err.Error())
+				return nil
+			}
+			defer func(openFile *os.File) {
+				_ = openFile.Close()
+			}(openFile)
+
+			imageResult, _, err := image.Decode(openFile)
+			if err != nil {
+				fmt.Println("some error on image.decode***" + err.Error())
+				return nil
+			}
+
+			img := canvas.NewImageFromImage(imageResult)
+			fyneWindow.SetContent(img)
+			time.Sleep(10 * time.Second)
+
 			return nil
-		}
+		})
+	}
 
-		fmt.Println(path)
-		openFile, err := os.Open(path)
-		if err != nil {
-			fmt.Println("some error on os.open***" + err.Error())
-			return nil
-		}
-		defer func(openFile *os.File) {
-			_ = openFile.Close()
-		}(openFile)
-
-		imageResult, _, err := image.Decode(openFile)
-		if err != nil {
-			fmt.Println("some error on image.decode***" + err.Error())
-			return nil
-		}
-
-		img := canvas.NewImageFromImage(imageResult)
-		fyneWindow.SetContent(img)
-		time.Sleep(10 * time.Second)
-
-		return nil
-	})
 	if err != nil {
 		return
 	}
